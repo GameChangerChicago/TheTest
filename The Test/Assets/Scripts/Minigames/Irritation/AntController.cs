50d6a2fa7cda40f70c5f9e5d0f6d2a258e59d3ad
mwarrington <mason.arrington@gmail.com>
2016-08-10 16:28:03 -0500

    Start of irritation game

---
+++ The Test/Assets/Scripts/Minigames/Irritation/AntController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntController : MonoBehaviour
+{
+    private Transform Cookie;
+    private bool _eating;
+
+    void Start()
+    {
+        Cookie = GameObject.Find("Cookie").transform;
+    }
+
+    void Update()
+    {
+        if(!_eating)
+        {
+            MoveTowardCookie();
+        }
+    }
+
+    private void MoveTowardCookie()
+    {
+        float goalAngle = Mathf.Atan((Cookie.position.y - this.transform.position.y) / (Cookie.position.x - this.transform.position.x));
+
+        
+
+        this.transform.Translate(Vector2.up * Time.deltaTime);
+    }
+}
