252c7b074cd82cd541494218dd623d2367e10df6
mwarrington <mason.arrington@gmail.com>
2016-08-12 18:24:01 -0500

    Cookie game almost done

--- The Test/Assets/Scripts/Minigames/Irritation/AntController.cs
+++ The Test/Assets/Scripts/Minigames/Irritation/AntController.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class AntController : MonoBehaviour
 {
-    private Transform Cookie;
+    protected bool eating
+    {
+        get
+        {
+            return _eating;
+        }
+        set
+        {
+            if(value != _eating)
+            {
+                _myAnimator.SetBool("Eating", value);
+                _eating = value;
+            }
+        }
+    }
     private bool _eating;
+    private Animator _myAnimator;
+    private Transform _cookie;
 
     void Start()
     {
-        Cookie = GameObject.Find("Cookie").transform;
+        _myAnimator = this.GetComponent<Animator>();
+        _cookie = GameObject.Find("Cookie").transform;
     }
 
     void Update()
     {
-        if(!_eating)
+        if(!eating)
         {
             MoveTowardCookie();
         }
@@ -21,10 +38,23 @@ public class AntController : MonoBehaviour
 
     private void MoveTowardCookie()
     {
-        float goalAngle = Mathf.Atan((Cookie.position.y - this.transform.position.y) / (Cookie.position.x - this.transform.position.x));
+        float goalAngle = Mathf.Rad2Deg * Mathf.Atan((this.transform.position.x - _cookie.position.x) / (_cookie.position.y - this.transform.position.y));
 
-        
+        if (goalAngle < 0)
+        {
+            goalAngle = 360 + goalAngle;
+        }
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, goalAngle));
 
         this.transform.Translate(Vector2.up * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Cookie")
+        {
+            eating = true;
+        }
     }
 }
