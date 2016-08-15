b63bb8fa5025b467911e068d5eb9ab83d5ffdec0
mwarrington <mason.arrington@gmail.com>
2016-08-15 15:50:20 -0500

    Cookie game works

--- The Test/Assets/Scripts/Minigames/Irritation/AntController.cs
+++ The Test/Assets/Scripts/Minigames/Irritation/AntController.cs
@@ -19,26 +19,53 @@ public class AntController : MonoBehaviour
         }
     }
     private bool _eating;
+    
     private Animator _myAnimator;
+    private CookieManager _theCookieManager;
     private Transform _cookie;
+    private bool _dragging;
 
     void Start()
     {
         _myAnimator = this.GetComponent<Animator>();
-        _cookie = GameObject.Find("Cookie").transform;
+        _theCookieManager = FindObjectOfType<CookieManager>();
+        _cookie = _theCookieManager.transform;
     }
 
     void Update()
     {
-        if(!eating)
+        eating = Physics2D.OverlapPoint(this.transform.position, 512);
+
+        if(_dragging)
+        {
+            Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.position = new Vector3(currentPosition.x, currentPosition.y, this.transform.position.z);
+        }
+        else if(!eating)
         {
             MoveTowardCookie();
+        }
+        else
+        {
+            EatingHandler();
         }
     }
 
     private void MoveTowardCookie()
     {
         float goalAngle = Mathf.Rad2Deg * Mathf.Atan((this.transform.position.x - _cookie.position.x) / (_cookie.position.y - this.transform.position.y));
+
+        if (_cookie.position.y < this.transform.position.y)
+        {
+            if (goalAngle > 180)
+            {
+                goalAngle -= 180;
+            }
+            else
+            {
+                goalAngle += 180;
+            }
+        }
 
         if (goalAngle < 0)
         {
@@ -50,11 +77,18 @@ public class AntController : MonoBehaviour
         this.transform.Translate(Vector2.up * Time.deltaTime);
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+    private void EatingHandler()
     {
-        if (col.tag == "Cookie")
-        {
-            eating = true;
-        }
+        _theCookieManager.PercentEaten += Time.deltaTime;
+    }
+
+    void OnMouseDown()
+    {
+        _dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        _dragging = false;
     }
 }
--- The Test/Assets/Scripts/Minigames/Irritation/CookieManager.cs
+++ The Test/Assets/Scripts/Minigames/Irritation/CookieManager.cs
@@ -4,4 +4,64 @@ using System.Collections;
 public class CookieManager : MonoBehaviour
 {
     public GameObject[] CookieSprites;
+    public float PercentEaten;
+
+    protected int currentCookieIndex
+    {
+        get
+        {
+            return _currentCookieIndex;
+        }
+        set
+        {
+            if(_currentCookieIndex != value)
+            {
+                for (int i = 0; i < CookieSprites.Length; i++)
+                {
+                    if(i != value)
+                    {
+                        CookieSprites[i].SetActive(false);
+                    }
+                    else
+                    {
+                        CookieSprites[i].SetActive(true);
+                    }
+                }
+
+                //AntController[] allAnts = FindObjectsOfType<AntController>();
+
+                //for (int i = 0; i < allAnts.Length; i++)
+                //{
+                //    allAnts[i].StartMoving();
+                //}
+
+                _currentCookieIndex = value;
+            }
+        }
+    }
+    private int _currentCookieIndex;
+
+    void Update()
+    {
+        if(PercentEaten > 99)
+        {
+            currentCookieIndex = 5;
+        }
+        else if(PercentEaten > 75)
+        {
+            currentCookieIndex = 4;
+        }
+        else if(PercentEaten > 50)
+        {
+            currentCookieIndex = 3;
+        }
+        else if (PercentEaten > 25)
+        {
+            currentCookieIndex = 2;
+        }
+        else if (PercentEaten > 10)
+        {
+            currentCookieIndex = 1;
+        }
+    }
 }
