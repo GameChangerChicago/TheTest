84664d5557832d6487873ec97d69728157f84ef6
mwarrington <mason.arrington@gmail.com>
2016-08-17 16:12:53 -0500

    Impassable objects for Fire Fly game

--- The Test/Assets/Scripts/Minigames/FindingFriends/PlayerFireFlyController.cs
+++ The Test/Assets/Scripts/Minigames/FindingFriends/PlayerFireFlyController.cs
@@ -5,7 +5,9 @@ public class PlayerFireFlyController : MonoBehaviour
 {
     private Rigidbody2D _playerRigidbody;
     private Vector2 _targetPosition;
-    private bool _atTarget;
+    private bool _atTarget,
+                 _colliding,
+                 _hasControl = true;
 
     void Start()
     {
@@ -20,9 +22,19 @@ public class PlayerFireFlyController : MonoBehaviour
             _targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !_colliding)
+        {
+            _hasControl = true;
+        }
+
         if (!_atTarget)
         {
-            PlayerMovement();
+            if (_hasControl)
+                PlayerMovement();
+            else
+            {
+                _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x / 1.1f, _playerRigidbody.velocity.y / 1.1f);
+            }
         }
         else if (Vector2.Distance(_playerRigidbody.position, _targetPosition) > 0.5f)
         {
@@ -44,7 +56,14 @@ public class PlayerFireFlyController : MonoBehaviour
     {
         if (col.tag == "Impassable")
         {
-            //_playerRigidbody.velocity = 
+            _playerRigidbody.velocity = new Vector2(-_playerRigidbody.velocity.x * 1.5f, -_playerRigidbody.velocity.y * 1.5f);
+            _colliding = true;
+            _hasControl = false;
         }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        _colliding = false;
     }
 }
