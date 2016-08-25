b340129b478d1bae939e106d36c0a2ce302f71ff
mwarrington <mason.arrington@gmail.com>
2016-08-25 15:58:56 -0500

    Firefly game optimization, the start of

---
+++ The Test/Assets/Scripts/Minigames/FindingFriends/FireFlyBackgroundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireFlyBackgroundManager : MonoBehaviour
+{
+    public GameObject Background1,
+                      Background2;
+    private List<GameObject> _allBackgrounds;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void BackgroundHandler()
+    {
+        if (_allBackgrounds.Count > 1)
+        {
+            for (int i = 0; i < _allBackgrounds.Count; i++)
+            {
+                //Things happen to these _allBackground[i]
+            }
+        }
+        else
+        {
+            //Do things to this ---> _allBackgrounds[0]
+        }
+    }
+}
