90427677c8422784ae1fea6146d56f833c38e209
mwarrington <mason.arrington@gmail.com>
2016-09-01 15:28:09 -0500

    New script with some psedocode
    I'll be making it actually do stuff tomorrow.

---
+++ The Test/Assets/Scripts/Minigames/FindingFriends/FireFlyFriendsManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireFlyFriendsManager : MonoBehaviour
+{
+    //list of vector2s
+    
+    void Start()
+    {
+        //populate list of vector2s
+    }
+    
+    //OntriggerEnter
+    //instantiate ff @ col.pos
+
+    //ONtriggerexit
+    //record col.pos and add it to list of vector2
+    //delete col.gameobject
+}
