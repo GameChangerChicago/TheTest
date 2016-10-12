d94ed8035ec0dfa0c00d72f86c443932fc22932e
Ash <kayura60@gmail.com>
2016-10-12 02:28:25 -0500

    Character Selection Screen + Music

---
+++ The Test/Assets/Scripts/Managers/CharacterSelector.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class CharacterSelector : MonoBehaviour
+{
+
+	public GameObject[] CharacterArray;
+	public Button PreviousButton, NextButton;
+	private int ArrayIndex;
+	private GameObject SelectedCharacter, PreviousCharacter;
+	public string CharacterName;
+	private GameManager _theGameManager;
+	private DialogManager _theDialogManager;
+
+	void Start ()
+	{
+		ArrayIndex = 0;
+		SelectedCharacter = CharacterArray [ArrayIndex];
+		SelectedCharacter.SetActive (true);
+		CharacterName = SelectedCharacter.ToString ();
+
+		_theGameManager = FindObjectOfType<GameManager> ();
+		_theDialogManager = FindObjectOfType<DialogManager> ();
+	}
+
+	public void PlayerSelectButton (string characterName)
+	{
+		if (CharacterName == "Marlon") {
+			GameManager.CurrentCharacterType = CharacterType.Marlon;
+		}
+		if (characterName == "Felix") {
+			GameManager.CurrentCharacterType = CharacterType.Felix;
+		} 
+		if (characterName == "Isaac") {
+			GameManager.CurrentCharacterType = CharacterType.Isaac;
+		}
+		GameManager.CharacterSelected = true;
+
+	}
+
+	public void ContinueToDialog ()
+	{
+		SceneManager.LoadScene ("MapScene");
+		//HACK: As pretty as this looks, this is a hack for a pre map game build
+		//if (GameManager.CurrentCharacterType == CharacterType.Marlon)
+		//{
+		//    SceneManager.LoadScene("RoomEscape");
+		//}
+		//else
+		//{
+		//    SimpleFrame.SetActive(false);
+		//    _theDialogManager.LoadPieceOfDialog();
+		//}
+	}
+
+	public void Update ()
+	{
+		Debug.Log (ArrayIndex); 
+	}
+
+	public void NextChoice ()
+	{
+		if (PreviousButton.interactable == false) {
+			PreviousButton.interactable = true;
+		}
+		
+		if (ArrayIndex < 2) {
+			ArrayIndex++;
+			PreviousCharacter = SelectedCharacter;
+			PreviousCharacter.SetActive (false);
+			SelectedCharacter = CharacterArray [ArrayIndex];
+			SelectedCharacter.SetActive (true);
+			CharacterName = SelectedCharacter.ToString ();
+		
+		}
+		if (ArrayIndex == 2) {
+			
+			NextButton.interactable = false;
+
+		}
+			
+	}
+
+	public void PreviousChoice ()
+	{
+		if (NextButton.interactable == false)
+			NextButton.interactable = true;
+			
+
+		if (ArrayIndex > 0) {
+			ArrayIndex--;
+			PreviousCharacter = SelectedCharacter;
+			PreviousCharacter.SetActive (false);
+			SelectedCharacter = CharacterArray [ArrayIndex];
+			SelectedCharacter.SetActive (true);
+			CharacterName = SelectedCharacter.ToString ();
+		} 
+
+		if (ArrayIndex == 0) {
+			ArrayIndex = 0;
+			PreviousButton.interactable = false;
+
+
+		}
+	}
+}
